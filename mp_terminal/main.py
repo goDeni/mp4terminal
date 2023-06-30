001094c9a24c7ca2120268496486f3b69fd5b1b8
goDeni <den4ikept11@gmail.com>
2023-07-01 01:02:20 +0500

    fixes

--- mp_terminal/main.py
+++ mp_terminal/main.py
@@ -1,43 +1,13 @@
 from argparse import ArgumentParser, FileType
 from os import get_terminal_size
 from sys import stdout
-from time import monotonic, sleep
-from typing import Generator
 
-from cursor import hide as hide_cursor
-from cursor import show as show_cursor
 from cv2 import VideoCapture
-from ffmpeg import probe
-from numpy import asarray, ndarray
-from PIL import Image
 from pydub import AudioSegment
 from simpleaudio import play_buffer
 
-_RESET_FG = "\x1b[0m"
-
-
-def _resize_image(image: ndarray, window_size: tuple[int, int]) -> ndarray:
-    return asarray(Image.fromarray(image).resize(window_size))
-
-
-def _read_video_frames(
-    video_path: str, size: tuple[int, int]
-) -> Generator[ndarray, None, None]:
-    vidcap = VideoCapture(video_path)
-    while True:
-        sucess, image = vidcap.read()
-        if not sucess:
-            break
-
-        yield _resize_image(image, size)
-
-
-def _format_time(seconds: float):
-    minutes = int(seconds / 60)
-    if minutes:
-        seconds -= minutes * 60
-
-    return f"{minutes:-02}:{int(seconds):-02}"
+from mp_terminal.terminal_player import TerminalPlayer
+from mp_terminal.video_meta import get_video_meta
 
 
 def _get_video_size(display_ratio: tuple[int, int], terminal_size: tuple[int, int]):
@@ -64,59 +34,40 @@ def _get_video_size(display_ratio: tuple[int, int], terminal_size: tuple[int, in
 
 def view_video(video_path: str):
     audio_segment = AudioSegment.from_file(video_path)
-
-    info = probe(video_path)
-    duration: float = float(info["streams"][0]["duration"])
-    frames_number: float = float(info["streams"][0]["nb_frames"])
-    frame_duration: int = duration / frames_number
+    meta = get_video_meta(video_path)
 
     terminal_size = get_terminal_size()
-    video_size = _get_video_size(
-        tuple(map(int, info["streams"][0]["display_aspect_ratio"].split(":"))),
-        (int(terminal_size.columns / 2), terminal_size.lines - 1),
+    resolution = _get_video_size(
+        meta.resolution,
+        (terminal_size.columns, terminal_size.lines),
     )
+    vidcap = VideoCapture(video_path)
 
-    print(chr(27) + "[2J", end="")
-    hide_cursor()
-    frames_generator = _read_video_frames(
-        video_path, size=(video_size[0] * 2, video_size[1])
+    terminal_player = TerminalPlayer(stdout, resolution, meta.fps, meta.duration)
+    playback = play_buffer(
+        audio_segment.raw_data,
+        num_channels=audio_segment.channels,
+        bytes_per_sample=audio_segment.sample_width,
+        sample_rate=audio_segment.frame_rate,
     )
+    terminal_player.start()
     try:
-        start_time = monotonic()
-        playback = play_buffer(
-            audio_segment.raw_data,
-            num_channels=audio_segment.channels,
-            bytes_per_sample=audio_segment.sample_width,
-            sample_rate=audio_segment.frame_rate,
-        )
-
-        frame_number = 0
         while True:
-            delta = (frame_duration * frame_number) - (monotonic() - start_time)
-            if delta > 0:
-                sleep(delta)
+            sucess, frame = vidcap.read()
+            if not sucess:
+                break
 
-            frame = next(frames_generator)
-            frame_number += 1
-
-            if delta < 0:
-                continue
-
-            height, width, _ = frame.shape
-            stdout.write(f"\x1b[{height + 1}A")
-            for height_idx in range(height):
-                for width_idx in range(width):
-                    blue, green, red = frame[height_idx][width_idx]
-                    stdout.write(f"\x1b[48;2;{red};{green};{blue}m ")
-                stdout.write(_RESET_FG + "\n")
-
-            stdout.write(_format_time(monotonic() - start_time) + "\n")
-            stdout.flush()
-    except StopIteration:
-        pass
+            terminal_player.render_frame(frame)
+    except KeyboardInterrupt:
+        terminal_player.reset(True)
+        raise
+    except:
+        terminal_player.reset(False)
+        raise
+    else:
+        terminal_player.reset(True)
     finally:
         playback.stop()
-        show_cursor()
 
 
 def _parse_args():
---
+++ mp_terminal/video_meta.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass
+from typing import Any, Dict
+
+from ffmpeg import probe
+
+
+
+@dataclass
+class VideoMeta:
+    duration: float
+    frames_number: int
+    resolution: tuple[int, int]
+    fps: float
+
+
+def _get_duration(video_stream_info: Dict[str, Any]) -> float:
+    if (duration := video_stream_info.get("duration")) is not None:
+        return float(duration)
+    
+    if (tags := video_stream_info.get("tags")) is not None:
+        if (duration := tags.get("DURATION")) is not None:
+            # 00:22:46.006000000
+            hours, minutes, seconds = duration.split(":", maxsplit=3)
+            return (int(hours) * 60 + int(minutes)) * 60 + float(seconds)
+    raise ValueError("Unable to find video duration")
+    
+
+def _get_frames_number(video_stream_info: Dict[str, Any]) -> int:
+    if (nb_frames := video_stream_info.get("nb_frames")) is not None:
+        return int(nb_frames)
+
+    if (tags := video_stream_info.get("tags")) is not None:
+        if (nb_frames := tags.get("NUMBER_OF_FRAMES")) is not None:
+            return int(nb_frames)
+
+    raise ValueError("Unable to find video frames number")
+
+def _get_resolution(video_stream_info: Dict[str, Any]) -> tuple[int, int]:
+    if (display_aspect_ratio := video_stream_info.get("display_aspect_ratio")) is not None:
+        return tuple(map(int, display_aspect_ratio.split(":")))
+
+    raise ValueError("Unable to find video resolution")
+
+def _get_fps(video_stream_info: Dict[str, Any]) -> float:
+    if (avg_frame_rate := video_stream_info.get("avg_frame_rate")) is not None:
+        x, y = map(int, avg_frame_rate.split('/', maxsplit=1))
+        return x / y
+
+    raise ValueError("Unable to find video resolution")
+
+def get_video_meta(video_path: str) -> VideoMeta:
+    info = probe(video_path)
+    
+    video_stream: Dict[str, Any] = info["streams"][0]
+    return VideoMeta(
+        duration=_get_duration(video_stream),
+        frames_number=_get_frames_number(video_stream),
+        resolution=_get_resolution(video_stream),
+        fps=_get_fps(video_stream)
+    )
+    
