642d3b191f87bbf78498c294ca790b08cc59563b
goDeni <den4ikept11@gmail.com>
2025-03-01 15:14:40 +0300

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate argparse;
+
+use argparse::{ArgumentParser, Store};
+use std::fs::File;
+use std::path::{Path, PathBuf};
+
+struct Args {
+    path: PathBuf,
+}
+
+fn parse_args() -> Args {
+    let mut path = String::new();
+    {
+        let mut parser = ArgumentParser::new();
+        parser
+            .refer(&mut path)
+            .required()
+            .add_argument("file", Store, "Vieo file path");
+
+        parser.parse_args_or_exit();
+    }
+    Args {
+        path: Path::new(path.as_str()).to_path_buf(),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = parse_args();
+    if !args.path.is_file() {
+        println!("File '{}' doesn't exists", args.path.to_str().unwrap());
+        return;
+    }
+
+    File::open(args.path).expect("Unable to open file");
 }
